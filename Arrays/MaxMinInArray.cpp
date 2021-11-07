99ca5fd1f1de075ef3fd83fddc3e2391c03afcf9
Rohit Satwadhar <rvsatwadhar@gmail.com>
2021-11-07 14:24:17 +0530

    Added Max Min in Array

---
+++ Arrays/MaxMinInArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int maxNum;
+int minNum;
+int main(){
+    
+    int arr[] = {1000, 11, 445, 1, 330, 0,-2};
+    int arr_size = 7;
+    maxNum=arr[0];
+    minNum=arr[0];
+
+    for(int i=1;i<arr_size;i++){
+        if(arr[i]>maxNum)maxNum=arr[i];
+
+        if(arr[i]<minNum)minNum=arr[i];
+    
+    }
+
+    cout<<"Max Num:\t"<<maxNum;
+    cout<<"\nMin Num:\t"<<minNum;
+    
+}
